592b45711dc46891cd5cdaf651ccf047cb8d83f7
Shailee Yadav <shaileeyadav130@gmail.com>
2023-04-13 20:08:17 +0530

    changes

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const path = require("path");
 require("dotenv").config();
 const port = process.env.PORT;
 
-const mongoose = require("mongoose");
-const profile=require('./routes/profile');
+// const mongoose = require("mongoose");
+// const profile=require('./routes/profile');
 const {readdirSync}=require('fs');
-readdirSync("./routes").map(file=>app.use('/',require(`./routes/${file}`)))
+readdirSync("./routes").map(file=>app.use('/',require(`./routes/${file}`)));;
 
 
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/portfolio-api", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log(" MONGODB CONNECTION OPEN AT ");
-  })
-  .catch((err) => {
-    console.log(`OH NO ERROR ! ${err}`);
-  });
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/portfolio-api", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log(" MONGODB CONNECTION OPEN AT ");
+//   })
+//   .catch((err) => {
+//     console.log(`OH NO ERROR ! ${err}`);
+//   });
 
   app.get('/',(req,res)=>{
     res.send("main page");
--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,13 @@
 const express=require('express');
-const Profile=require('../models/profileSchema')
 
 const router=express.Router();
-router.get('/profile', async(req,res)=>{
-    const profile = new Profile({
-        name: "Shailee Yadav",
-        email: "shaileeyadav130@gmail.com",
-        phoneNo: 7348265799,
-      });
-      await profile.save();
-      res.send(profile);
-})
+
+const profile = {
+    "name": "Shailee Yadav",
+    "email": "shaileeyadav130@gmail.com",
+    "phoneNo": 7348265799,
+  }
+router.get('/profile', (req,res)=>{
+    res.send(profile);
+});
 module.exports=router;
