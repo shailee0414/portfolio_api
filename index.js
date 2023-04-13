309e43606de4b464c42d2225cf39580134f4044c
Shailee Yadav <shaileeyadav130@gmail.com>
2023-04-13 15:30:01 +0530

    adding some file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port= process.env.PORT || 8000;
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
-const Profile=require("./models/profile")
+const Profile = require("./models/profile");
+const port = process.env.PORT;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/portfolio-api", {
@@ -16,18 +18,23 @@ mongoose
   })
   .catch((err) => {
     console.log(`OH NO ERROR ! ${err}`);
-});
+  });
 
-app.get('/profile',async (req,res) => {
-    const profile=new Profile({
-        name:"Shailee Yadav",
-        email:"shaileeyadav130@gmail.com",
-        phoneNo:7348265799
-    })
-    await profile.save();
-    res.send(profile);
+  app.get('/',(req,res)=>{
+    res.send('main page');
+  })
+app.get("/profile", async (req, res) => {
+  const profile = new Profile({
+    name: "Shailee Yadav",
+    email: "shaileeyadav130@gmail.com",
+    phoneNo: 7348265799,
+  });
+  await profile.save();
+  res.send(profile);
 });
 
 app.listen(port, () => {
-  console.log("app on 3000 port");
+  console.log("app on  port",port);
 });
+
+module.exports = app;
