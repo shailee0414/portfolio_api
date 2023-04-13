76e45392e9fd3499203501956a9b3e968d39682d
Shailee Yadav <shaileeyadav130@gmail.com>
2023-04-13 22:26:32 +0530

    adding more data in profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,25 @@
-const express=require('express');
+const express = require("express");
 
-const router=express.Router();
+const router = express.Router();
 
 const profile = {
-    "name": "Shailee Yadav",
-    "email": "shaileeyadav130@gmail.com",
-    "phoneNo": 7348265799,
-  }
-router.get('/profile', (req,res)=>{
-    res.send(profile);
+  name: "Shailee Yadav",
+  email: "shaileeyadav130@gmail.com",
+  phoneNo: 7348265799,
+  about:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+  linkedin: "https://www.linkedin.com/in/shailee-yadav/",
+  github: "https://github.com/shailee0414",
+  academic: [
+    {
+      college: {
+        branch: "Electronics Engineering",
+        percentage: "75",
+      },
+    },
+  ],
+};
+router.get("/profile", (req, res) => {
+  res.send(profile);
 });
-module.exports=router;
+module.exports = router;
